79da080b8fdedc52c2d51994503d28bc295cd594
John Gilliland <johncgilliland@gmail.com>
2016-06-11 21:20:19 -0700

    first modules

---
+++ SieveOfEratosthenes/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace SieveOfEratosthenes.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [ImportingConstructor]
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
